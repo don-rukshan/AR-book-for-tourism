6faba96810f321d98e492500a0dd7c5c983a3567
don-rukshan <md.rukshan@gmail.com>
2020-02-21 15:21:56 +0530

    Complete Project

---
+++ Assets/Vuforia/Scripts/menustart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menustart : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void changemenuscene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
